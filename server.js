78b1a7fb1fa5c15b44f24bab9e2fc1bbb093f2fc
Sufia <sufia.begum@highpeaksw.com>
2023-09-30 10:35:14 -0400

    new updates

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-var express = require('express')
+var express = require("express");
 
-var app = express()
+var app = express();
 
-const SERVER_PORT = 3000
-const SERVER_HOST = "localhost"
+const SERVER_PORT = 3000;
+const SERVER_HOST = "localhost";
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static("views"));
 //http://localhost:3000/
-app.get('/', function (req, res) {
-    res.send("<h1>Home Page</h1>")
-})
+app.get("/", function (req, res) {
+  res.sendFile("./views/index.html");
+});
 
 //http://localhost:3000/profile
-app.post('/profile', (req, res) => {
-  console.log(req.body)
-  res.json(req.body)
-})
+app.get("/profile", (req, res) => {
+  // console.log(req.body);
+  // res.json(req.body);
+  res.send("User Homepage");
+});
 
 //http://localhost:3000/admin
-app.get('/admin', (req, res) => {
-  res.send('Admin Homepage')
-})
+app.get("/admin", (req, res) => {
+  res.send("Admin Homepage");
+});
 
 //http://localhost:3000/user/100
-app.get("/user/:id", (req, res)=> {
-      res.send(`User ID: ${req.params.id}`);
-    }
-)
+app.get("/user/:id", (req, res) => {
+  res.send(`User ID: ${req.params.id}`);
+});
 
 //http://localhost:3000/valueofday/1980-01-24
 app.get("/valueofday/:year(\\d{4})-:month(\\d{2})-:day(\\d{2})", (req, res) => {
-  console.log(req.params)
-  res.send(req.params)
+  console.log(req.params);
+  res.send(req.params);
 });
-  
-app.listen(process.env.PORT  || SERVER_PORT, () => {
-    console.log(`Server running at http://${SERVER_HOST}:${SERVER_PORT}/`);
-})
+
+app.listen(process.env.PORT || SERVER_PORT, () => {
+  console.log(`Server running at http://${SERVER_HOST}:${SERVER_PORT}/`);
+});
