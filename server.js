9f56002efeb51293b09002d3cb40a5d2b7ed6722
Vijayaniraula90 <119446070+Vijayaniraula90@users.noreply.github.com>
2023-09-30 13:38:24 -0400

    add new url /name

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
+//http://localhost:3000/
+app.get('/name', function (req, res) {
+    res.send("<h1>VIJAYA NIRAULA</h1>")
+})
+
+
+
 //http://localhost:3000/profile
 app.post('/profile', (req, res) => {
   console.log(req.body)
