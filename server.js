af8097307842d6df326a77bc95ebb623da99673a
Jscheema56 <157239825+Jscheema56@users.noreply.github.com>
2024-02-17 13:42:17 -0500

    name

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
+//http://localhost:3000/name
+app.get('/name', function (req, res) {
+  res.send("<h1>c0866930 - Jaideep singh</h1>")
+})
+
 //http://localhost:3000/profile
 app.post('/profile', (req, res) => {
   console.log(req.body)
